d342e2a7bfdb831da5ba610ce3bc634958f09660
zenodavids <chidozieezeanekwe@gmail.com>
2022-09-28 15:39:00 +0100

    add to opeansea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x05a34185EEe61F052A9A3b4d3338c928e9A3CC6B";
+const WHITELIST_CONTRACT_ADDRESS = "0xaBEAd78fCA1fB19692a2f5141C162839fD29Edd7";
 // URL to extract Metadata for a Crypto Dev NFT
 // this url is the deployed website url for this web app
-const METADATA_URL = "https://nft-presale-collection.vercel.app/";
+const METADATA_URL = "https://nft-presale-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
